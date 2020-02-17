2063d38c0d47f87080c82d00fa5d6efe0a66a988
marciohass <marciohhss@gmail.com>
2020-02-17 17:54:30 -0300

    subindo todos os arquivos do curso de angular

---
+++ hello-typescript/main.ts
@@ -0,0 +1,23 @@
+var minhaVar = 'minha variavel';
+
+function minhaFunc(x, y){
+    return x + y;
+}
+
+let num = 2;
+const PI = 3.14;
+
+var numeros = [1,2,3];
+numeros.map(function(valor){
+    return valor * 2;
+});
+numeros.map(valor => valor * 2);
+
+class matematica{
+    soma(x,y){
+        return x + y;
+    }
+}
+
+var n1: any = 'dasdas';
+n1 = 4;
